4c9b97350b3f2ef8ae7be597a1e01eb009c30364
bendressouarnaud <97701973+bendressouarnaud@users.noreply.github.com>
2026-01-24 00:00:26 +0100

    Update Quartier.java

--- src/main/java/com/cnmci/core/model/Quartier.java
+++ src/main/java/com/cnmci/core/model/Quartier.java
@@ -3,6 +3,7 @@ package com.cnmci.core.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.util.Collection;
