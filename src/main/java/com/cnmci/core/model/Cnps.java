2c723d3fe658336179216c3eed40b2f90eb5a8c9
ankk <bendressouarnaud@gmail.com>
2026-05-17 22:48:36 +0200

    Cnps Updated

--- src/main/java/com/cnmci/core/model/Cnps.java
+++ src/main/java/com/cnmci/core/model/Cnps.java
@@ -2,6 +2,7 @@ package com.cnmci.core.model;
 
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
@@ -9,6 +10,7 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 @Entity
+@NoArgsConstructor
 public class Cnps extends AbstractEntity{
     private Long artisanId;
     private Long apprentiId;
